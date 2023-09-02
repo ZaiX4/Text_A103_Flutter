5aa31f6f215d0d8fbe3d9c43a760cf179a2a21e2
yunzaixi <yzx.star@outlook.com>
2023-09-02 15:59:01 +0800

    第1次提交

---
+++ README.md
@@ -0,0 +1,6 @@
+# test_a103
+
+这是一个由flutter实现的安卓应用,仅供测试使用,请勿泄露
+
+## Getting Started
+
---
+++ android/app/src/main/kotlin/com/example/test_a103/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_a103
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
