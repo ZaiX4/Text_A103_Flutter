add561b6e7bce081286cf88620a1f8eddca4fe6e
yunzaixi <yzx.star@outlook.com>
2023-09-05 17:11:49 +0800

    A101正式版本

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_displaymode/flutter_displaymode.dart';
-import 'ui/output_ui/output_ui.dart';
+import 'ui/output_ui/output_ui.dart' as output;
 import 'ui/input_ui/input_ui.dart';
 
 import 'package:provider/provider.dart';
@@ -9,6 +9,8 @@ import 'package:provider/provider.dart';
 void main() {
   FlutterDisplayMode.setHighRefreshRate();
   //以界面为基础创建activity
+  output.all_chat_ls.add_f(output.chat_bubble(text:"版本号A101",id:0));
+  output.all_chat_ls.add_f(output.text_divider(text: "这只是一条分割线",));
   runApp(main_screen());
 }
 
@@ -19,7 +21,7 @@ class main_screen extends StatelessWidget {
     return MaterialApp(
       initialRoute: '/output',
       routes: {
-        '/output': (context) => output_ui(),
+        '/output': (context) => output.output_ui(),
         '/input': (context) => input_ui(),
       },
     );
